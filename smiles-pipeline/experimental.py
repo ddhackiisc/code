8b6f47008e5c39aea16381398e3ee1b8a4ba9b16
jrd99 <julianrdcosta@gmail.com>
2020-07-28 21:11:54 +0530

    smiles-pipeline/data/readme.md

--- smiles-pipeline/experimental.py
+++ smiles-pipeline/experimental.py
@@ -8,11 +8,6 @@ Created on Mon Jul 27 17:21:32 2020
 This file is for visualizing molecules in the SMILES format using rdkit
 CCOC(=O)C(O)(C1=CC=C(Cl)C=C1)C2=CC=C(Cl)C=C2
 """
-from rdkit import Chem, DataStructs
-from rdkit.Chem import Draw, Descriptors, AllChem
-import numpy as np
-import pandas as pd
-
 """Useful commands
 mol = Chem.MolFromSmiles('CCCO')
 smiles = Chem.MolToSmiles(mol)
@@ -44,5 +39,102 @@ DataStructs.TanimotoSimilarity(glyanalyse,cyanalyse)
 #apparently pickling molecules is much faster than 
 #reparsing SMILES strings
 """
-data = pd.read_csv('data/nr-ar.smiles', sep='\t',names=['SMILES','Identifier','Activity'])
-#df[df['Activity']==1]
+from rdkit import Chem, DataStructs
+from rdkit.Chem import Draw, Descriptors, AllChem
+
+import numpy as np
+import pandas as pd
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
+
+data = pd.read_csv('data/nr-ar.smiles',
+                   sep='\t',
+                   names=['SMILES','Identifier','Activity'])
+minipos = data[data['Activity']==1].head()
+minineg = data[100:105]
+minidata = pd.concat([minipos,minineg])
+
+def validSMILES(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return False
+    return True
+
+def morganArrayFromSmiles(smiles, nBits):
+    """Accepts a SMILES string and a size parameter nBits and
+    produces a numpy array of the required size"""
+    RADIUS = 2
+    mol = Chem.MolFromSmiles(smiles)
+    fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius=RADIUS, nBits=nBits)
+    arr = np.zeros((1,))
+    DataStructs.ConvertToNumpyArray(fp, arr)
+    return arr
+    
+def createDataset(df):
+    """takes a pandas dataframe with smiles and a target and creates the 
+    X feature matrix and y target vector to train an ML models"""
+    df['Valid'] = df['SMILES'].apply(validSMILES)
+    cleandf = df[df['Valid']==True]
+    X = [morganArrayFromSmiles(sm,2048) for sm in cleandf['SMILES']]
+    X = np.array(X)
+    y = np.array(cleandf['Activity'])
+    assert len(X)==len(y)
+    return X,y
+
+X,y = createDataset(data)
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, random_state=1)
+
+model = LogisticRegression(class_weight='balanced')
+#model = LogisticRegression()
+#model = GaussianNB()
+model.fit(Xtrain, ytrain)                  
+y_model = model.predict(Xtest)   
+ytrain_model = model.predict(Xtrain)
+print('Train accuracy: ',accuracy_score(ytrain, ytrain_model))
+print('Test accuracy: ',accuracy_score(ytest, y_model))
+print('F1 score: ',f1_score(ytest, y_model))
+
+"""Comments
+with 50 bits in the fingerprint
+A Gaussian Naive Bayes Model gets to 84 percent accuracy 
+but this is meaningless. F1 score is 0.2
+
+with 2048 bits 
+GNB
+Train accuracy:  0.8306968790081232
+Test accuracy:  0.791025641025641
+F1 score:  0.12834224598930483
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
